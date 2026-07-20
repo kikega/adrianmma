7f64c237d18bac15f9f36521e227708e29de5766
Enrique Gálvez <kikega@telefonica.net>
2026-07-20 18:13:25 +0200

    error deploy

--- deploy/gunicorn.conf.py
+++ deploy/gunicorn.conf.py
@@ -9,13 +9,6 @@ bind = os.getenv(
     "unix:/run/adrian/adrian.sock",
 )
 
-#workers = int(
-    os.getenv(
-        "GUNICORN_WORKERS",
-        multiprocessing.cpu_count(),
-    )
-)
-
 workers = os.getenv('GUNICORN_WORKERS', '3')
 
 worker_class = "sync"
